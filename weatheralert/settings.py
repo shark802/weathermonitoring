8a974c1a32042c41e67f5648c5b6ec5dba0f3354
Kyle <rolandkylejandog064@gmail.com>
2025-10-29 13:49:54 +0800

    Update to main

--- weatheralert/settings.py
+++ weatheralert/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-d0a@+xqkrda!+gb$6huxl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'py-server.pagenet.info,192.168.3.6,localhost,127.0.0.1,bccweatherapp-8fcc2a32c70f.herokuapp.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'py-server.pagenet.info,192.168.3.6,localhost,127.0.0.1,weatherappsys-7517da4b1dd4.herokuapp.com').split(',')
 
 
 # Application definition
@@ -186,7 +186,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://192.168.3.6',
     'http://localhost',
     'http://127.0.0.1',
-    'http://bccweatherapp-8fcc2a32c70f.herokuapp.com'
+    'https://weatherappsys-7517da4b1dd4.herokuapp.com'
 ]
 
 # Media files
